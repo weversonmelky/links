4e8257d455564c7065b14dd961c6aa9a71afde5c
weversonmelky <107736909+weversonmelky@users.noreply.github.com>
2024-04-07 15:32:49 -0400

    change footer

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function tooglemode(){
+function tooglemode() {
   const html = document.documentElement
 
-  const img = document.querySelector('#profile img')
-  html.classList.toggle('light')
-  if(html.classList.contains('light')){
+  const img = document.querySelector("#profile img")
+  html.classList.toggle("light")
+  if (html.classList.contains("light")) {
     img.setAttribute("src", "assets/avatar-light.png")
     img.setAttribute(
       "alt",
       "Personagem Frieren do anime Frieren e a Jornada para o Além, com  o ceu de dia"
     )
-  }else{
+  } else {
     img.setAttribute("src", "assets/avatar.png")
     img.setAttribute(
       "alt",
