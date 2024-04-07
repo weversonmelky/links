3c606de9557bb799d50d63d35c8eb3ea80237118
Weverson Melky <weversonmelky@gmail.com>
2024-04-07 14:24:20 -0400

    add new atribut in #profile>img>alt

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ function tooglemode(){
   html.classList.toggle('light')
   if(html.classList.contains('light')){
     img.setAttribute("src", "assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Personagem Frieren do anime Frieren e a Jornada para o Além, com  o ceu de dia"
+    )
   }else{
     img.setAttribute("src", "assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Personagem Frieren do anime Frieren e a Jornada para o Além, com  o ceu de noite"
+    )
   }
 }
