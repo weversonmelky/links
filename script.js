1c141340d7623d3b9344ec9bbbc1aa4afaccdbdc
unknown <weversonmelky@gmail.com>
2024-04-07 01:05:23 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function tooglemode(){
+  const html = document.documentElement
+
+  const img = document.querySelector('#profile img')
+  html.classList.toggle('light')
+  if(html.classList.contains('light')){
+    img.setAttribute("src", "assets/avatar-light.png")
+  }else{
+    img.setAttribute("src", "assets/avatar.png")
+  }
+}
